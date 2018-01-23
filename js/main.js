a48ad2218469cf60faaa2648696d968ac9628a32
namantiwari07 <namantiwari2015@gmail.com>
2018-01-23 02:24:14 -0500

    Master-commit

---
+++ js/main.js
@@ -0,0 +1,49 @@
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+});
+$(function () {
+    $('[data-toggle="popover"]').popover({
+        trigger: 'hover',
+        cursor: 'pointer',
+    });
+});
+
+
+
+
+// // $(function () {
+    
+//  $('.basic-addons1').hide();
+//  $('.basic-addons2').hide();
+//  $('.basic-addons3').hide();
+//  $('.basic-addons4').hide();
+    
+
+//     var errormessage = false;
+ 
+//      //focusin function
+
+//      $(".group1").focusin(function () {
+
+//          $('.form').css ({
+//              'border': '2px solid lightgrey',
+//              'border-radius': '10px',
+//              });
+//          $('.groupLabel1').css({
+//               'background-color':'rgba(211,211,211,0.3)',
+//              });     
+//        $('.basic-addons1').show();
+       
+//        $("#input1").attr("placeholder", "English ");
+       
+            
+
+// });
+
+
+              
+        
+
+
+
+
